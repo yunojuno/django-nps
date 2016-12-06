8b0ee106017c1e2bf4341d0b2cd8501edca82c77
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-12-06 09:15:00 +0000

    Bump version to v0.3.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-nps",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     install_requires=['Django>=1.8'],
     include_package_data=True,
